f4dcf7958170d14318a8b34f961b2b3d485fab5c
ilovearsenal93 <ongsatthu@gmail.com>
2014-07-14 09:14:53 +0700

    Modeling trainee and supervisor

---
+++ app/controllers/supervisors_controller.rb
@@ -0,0 +1,19 @@
+class SupervisorsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
